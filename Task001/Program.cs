a4143a67ac27ed0d6e622c8e9eb276a400a64a28
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-21 14:41:14 +0300

    add Math.Pow function in Task001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая на вход принимает число и выдаёт его квадрат (число, умноженное на само себя)
+int x = new Random().Next (1, 100);
+int stepen = 2;
+Console.WriteLine(x);
+Console.WriteLine(Math.Pow(x, stepen));
