781520bfd1972364b4854bee2354c7dd8c117149
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-21 14:52:45 +0300

    add user number edit in Task001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,5 +1,8 @@
 ﻿// Написать программу, которая на вход принимает число и выдаёт его квадрат (число, умноженное на само себя)
-int x = new Random().Next (1, 100);
+Console.Write("Write your number: ");
+//int x = Console.ReadLine ();
+int x = Convert.ToInt32(Console.ReadLine ());
+//int x = new Random().Next (1, 100);
 int stepen = 2;
 Console.WriteLine(x);
 Console.WriteLine(Math.Pow(x, stepen));
