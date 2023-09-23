1f1bc46c848c029e99e06435c76bb09229fdd35f
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-24 03:35:40 +0400

    create Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и
+// на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Write number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 0)
+{
+    x *= -1;
+}
+if (x >= 100 && x <= 999)
+{
+    int last = x % 10;
+    Console.Write(last);
+}
+else
+{
+    Console.Write("Error! Use 3 point number");
+}
