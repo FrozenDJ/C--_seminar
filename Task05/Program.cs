5e25fcb6da4a6eb6d7b37e66a7db0038141a4556
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-24 03:12:59 +0400

    create Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу, которая на вход принимает одно целое число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.Write("Write your number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = -n;
+while(count<=n)
+{
+    Console.Write($"{count} ");
+    count++;
+}
