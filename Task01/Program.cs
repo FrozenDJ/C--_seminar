f2346885d39d46af21e8af81c8b0a0942565e336
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-24 01:35:08 +0400

    create Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая на вход принимает 2 числа и 
+// проверяет, является ли первое число квадратом второго
+
+Console.Write("Write first number: ");
+int x = Convert.ToInt32(Console.ReadLine ());
+Console.Write("Write second number: ");
+int y = Convert.ToInt32(Console.ReadLine ());
+if (x == y * y) 
+{
+    Console.WriteLine($"{x} / {y} = {y} -> True");
+}
+else
+{
+    Console.WriteLine($"{x} / {y} = {y} -> Wrong");
+}
