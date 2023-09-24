e871e9fec6ac8ecaafecceed552e2c5da7280c8f
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-24 04:00:31 +0400

    create Task02(HW)

---
+++ Task02(HW)/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+Console.Write("Write first number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Write second number: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > y)
+{
+    Console.WriteLine($"{x} > {y} -> max = {x}");
+}
+else if (x == y)
+{
+    Console.WriteLine($"{x} = {y} Try again");
+}
+else
+{
+    Console.WriteLine($"{x} < {y} -> max = {y}");
+}
