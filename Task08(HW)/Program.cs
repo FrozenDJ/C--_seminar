2b10744397016516bbef661147d169ed3ebce425
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-24 05:23:48 +0400

    create Task08(HW)

---
+++ Task08(HW)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Write your number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 0)
+{
+    Console.Write("Error. Use only positive numbers");
+}
+int count = 1;
+while (count <= n)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count++;
+}
