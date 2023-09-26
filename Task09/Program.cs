5e3e6789e965312a522cc805d348d01d4beef18b
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-27 01:07:53 +0400

    add own method in Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -5,15 +5,24 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine(number);
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-if (firstDigit > secondDigit)
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if (firstDigit > secondDigit)
+// {
+//     Console.WriteLine($"max -> {firstDigit}");
+// }
+// else
+// {
+//     Console.WriteLine($"max -> {secondDigit}");
+// }
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"max -> {maxDigit}");
+
+int MaxDigit (int num)
 {
-    Console.WriteLine($"max -> {firstDigit}");
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
 }
-else
-{
-    Console.WriteLine($"max -> {secondDigit}");
-}
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+int maxDigit=MaxDigit(number);
 Console.WriteLine($"max -> {maxDigit}");
