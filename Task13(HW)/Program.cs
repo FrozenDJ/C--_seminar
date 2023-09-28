64c1a8c39dac830a6566b2c3bdfae0c04db1e7e1
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-28 18:19:14 +0400

    create Task13

---
+++ Task13(HW)/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Write your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number.Length;
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"{number} -> {count[2]}");
+}
+Console.WriteLine($"{number} -> No third digit");
