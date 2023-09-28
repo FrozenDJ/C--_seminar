b33c45b54e296ab87c80403879e7feef17b9c802
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-28 17:56:35 +0400

    create Task10

---
+++ Task10(HW)/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Write your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int ShowSecondDigit(int num)
+{
+    int lastDigit = num % 100;
+    return lastDigit / 10;
+}
+
+if (number < 0)
+{
+    number *= -1;
+}
+if (number >= 100 && number <= 999)
+{
+    int requiredDigit = ShowSecondDigit(number);
+    Console.WriteLine($"{number} -> {requiredDigit}");
+}
+else
+{
+    Console.Write("Error. Write three-digit number");
+}
