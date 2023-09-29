3d0032550c42874391c68600c486838b71bc08a4
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-29 18:53:25 +0400

    create Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт
+// таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Write your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number *= -1;
+}
+
+void TableSquare(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count,3} -- {count * count,5}");
+        count++;
+    }
+}
+TableSquare(number);
