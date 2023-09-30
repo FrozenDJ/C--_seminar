2bff20fed4480aa4223c337e90fbe3da4345005c
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-30 23:55:17 +0400

    create Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и 
+// выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Write your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int CountOfNumber(int num)
+{
+    if (number == 0) return 1;
+    int count = 0;
+    while (num != 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+int res = CountOfNumber(number);
+Console.WriteLine($"{number} -> {res}");
