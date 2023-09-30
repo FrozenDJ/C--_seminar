83c0cc4ae6492ebf7c8f11cc17cdf3a9e1e6ded2
Roman_Konkov <roman.konkoff@gmail.com>
2023-09-30 23:29:53 +0400

    create Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 24: Напишите программу, которая принимает 
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Write your positive integer number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 1)
+{
+    Console.WriteLine("Error. Incorrect number");
+    return;
+}
+
+int SumNumber(int num)
+{
+    int count = 1;
+    int sum = 0;
+    while (count <= num)
+    {
+        checked
+        {
+            sum = sum + count;
+            count++;
+        }
+    }
+    return sum;
+}
+
+int res = SumNumber(a);
+Console.WriteLine($"{a} -> {res}");
