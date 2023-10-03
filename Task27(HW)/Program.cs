8bbb4935188be98d8e0c07336c44f19f7ff2d247
Roman_Konkov <roman.konkoff@gmail.com>
2023-10-03 22:30:07 +0400

    create Task27(HW)

---
+++ Task27(HW)/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Write your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 0)
+{
+    Console.WriteLine("Error. Incorrect number");
+    return;
+}
+
+int SumDigits(int num)
+{
+    if (num < 0)
+    {
+        num *= -1;
+    }
+
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+int result = SumDigits(number);
+Console.WriteLine($"{number} -> {result}");
