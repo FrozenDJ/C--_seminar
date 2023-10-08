250e0283922fd2a944ceae9b73b2f8208db343c9
Roman_Konkov <roman.konkoff@gmail.com>
2023-10-08 22:59:59 +0400

    edit tasks 00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,8 +1,7 @@
-﻿// Написать программу, которая на вход принимает число и выдаёт его квадрат (число, умноженное на само себя)
+﻿// Написать программу, которая на вход принимает число и выдаёт его 
+// квадрат (число, умноженное на само себя)
 Console.Write("Write your number: ");
-//int x = Console.ReadLine ();
 int x = Convert.ToInt32(Console.ReadLine ());
-//int x = new Random().Next (1, 100);
 int stepen = 2;
 Console.WriteLine(x);
 Console.WriteLine(Math.Pow(x, stepen));
