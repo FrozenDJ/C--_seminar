deddf2440e89dd7a57f68377dc439b45c3ca51b3
Roman_Konkov <roman.konkoff@gmail.com>
2023-10-20 21:04:21 +0400

    create Task53

---
+++ Task53/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив. Напишите программу, которая 
+// поменяет местами первую и последнюю строку массива.
+
+int[,] CreateMatrixRndInt(int min, int max, int rows, int columns)
+{
+    //                         0    1
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void RepaceFirstLastRows(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] result = CreateMatrixRndInt(1, 10, 3, 4);
+PrintMatrix(result);
+Console.WriteLine();
+RepaceFirstLastRows(result);
+PrintMatrix(result);
