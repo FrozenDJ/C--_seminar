ac1851048f5e66e45fb498e549dafa9932836bbd
Roman_Konkov <roman.konkoff@gmail.com>
2023-10-21 17:33:28 +0400

    create Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет принимать на вход 
+// число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9;
+
+Console.Write("Write your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigitOfNumber(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SumDigitOfNumber(num / 10);
+}
+Console.Write($"{number} -> {SumDigitOfNumber(number)}");
