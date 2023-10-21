b8390a70dc558b3bbd3ee32bc3d99986f7d65dfa
Roman_Konkov <roman.konkoff@gmail.com>
2023-10-21 15:36:33 +0400

    create Task58(HW)

---
+++ Task58(HW)/Program.cs
@@ -0,0 +1,107 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет
+// находить произведение двух матриц.
+
+using System;
+
+public class Answer
+{
+    public static void MultiplyIfPossible(int[,] matrixA, int[,] matrixB)
+    { // Введите свое решение ниже
+        if (matrixA.GetLength(1) != matrixB.GetLength(0))
+        {
+            Console.WriteLine("It is impossible to multiply.");
+            return;
+        }
+        else PrintMatrix(MatrixMultiplication(matrixA, matrixB));
+    }
+
+    public static int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+    {  // Введите свое решение ниже
+        // c = a * b
+        // |a1 a2| * |b1 b2| = |c1 c2| = |a1*b1 a2*b2|
+        // |a3 a4|   |b3 b4|   |c3 c4|   |a3*b3 a4*b4|
+        int rowsA = matrixA.GetLength(0);
+        int colsA = matrixA.GetLength(1);
+        int colsB = matrixB.GetLength(1);
+        int[,] newMatrix = new int[rowsA, colsB];
+
+        for (int i = 0; i < rowsA; i++)
+        {
+            for (int j = 0; j < colsB; j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < colsA; k++)
+                {
+                    sum += matrixA[i, k] * matrixB[k, j];
+                }
+                newMatrix[i, j] = sum;
+            }
+        }
+        return newMatrix;
+    }
+
+    public static void PrintMatrix(int[,] matrix)
+    {  // Введите свое решение ниже
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"{matrix[i, j]}" + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+    // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args)
+    {
+        int[,] matrix;
+
+        if (args.Length == 0)
+        {
+            // Если аргументы не переданы, используем матрицу по умолчанию
+            matrix = new int[,]
+            {
+                {5, 6, 1},
+                {8, 5, 3},
+                {7, 2, 9}
+            };
+        }
+        else
+        {
+            // Иначе, парсим аргументы в двумерный массив
+            string[] rows = args[0].Split(';');
+            matrix = new int[rows.Length, rows[0].Split(',').Length];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                string[] elements = rows[i].Split(',');
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    if (int.TryParse(elements[j], out int number))
+                    {
+                        matrix[i, j] = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ошибка при парсинге аргумента {elements[j]}.");
+                        return;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("Исходная матрица:");
+        PrintMatrix(matrix);
+
+        int[,] matrixB = {
+            {5, 6, 4},
+            {7, 8, 1},
+            {1, 4, 5}
+        };
+
+        Console.WriteLine("\nMatrix B:");
+        PrintMatrix(matrixB);
+        
+        Console.WriteLine("\nMultiplication result:");
+        MultiplyIfPossible(matrix, matrixB);
+    }
+}
